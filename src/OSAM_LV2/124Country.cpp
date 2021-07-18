4a54e01dd39ec4313e0e6ae81afcde419fb39fa1
이상훈 <dltkdgns830@naver.com>
2021-07-18 10:46:35 +0000

    210718

---
+++ src/OSAM_LV2/124Country.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+
+    string answer = "";
+    int share = n;
+    int remainder;
+
+    while (share != 0) {
+        remainder = share % 3;
+        share = share / 3;
+
+        if (remainder == 0) { 
+            answer = "4" + answer;
+            share--;
+        }
+        else if (remainder == 1) {
+            answer = "1" + answer;
+        }
+        else if (remainder == 2) {
+            answer = "2" + answer;
+        }
+    }
+
+    return answer;
+}
